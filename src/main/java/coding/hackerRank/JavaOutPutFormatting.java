6c65b875c163db4a88b5173d7ed1547093d24fe0
Mohamed <mosamag96@gmail.com>
2024-10-29 22:36:27 +0530

    Hacker Rank - Output Format Challenge

--- src/main/java/coding/hackerRank/JavaOutPutFormatting.java
+++ src/main/java/coding/hackerRank/JavaOutPutFormatting.java
@@ -33,7 +33,7 @@ import java.util.Scanner;
 public class JavaOutPutFormatting {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("================================");
+		System.out.println("=================================");
 		for (int i = 0; i < 3; i++) {
 			String s1 = sc.next();
 			int x = sc.nextInt();
@@ -50,7 +50,7 @@ public class JavaOutPutFormatting {
 			else
 				System.out.println(x);
 		}
-		System.out.println("================================");
+		System.out.println("=================================");
 		sc.close();
 	}
 }
